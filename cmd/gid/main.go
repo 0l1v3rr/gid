94a927041db0e4308e462d6b0f94b091960be4e4
Olivér <oliver.mrakovics@gmail.com>
2022-01-01 14:33:11 +0100

    reading the input from the stdin

--- cmd/gid/main.go
+++ cmd/gid/main.go
@@ -1,5 +1,47 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
 
+func main() {
+	title()
+	reader := bufio.NewReader(os.Stdin)
+
+	message("$", "Please specify the URL: ")
+	prompt()
+	url, _ := reader.ReadString('\n')
+	url = strings.TrimSuffix(url, "\n")
+
+	cleanurl := strings.ReplaceAll(strings.ReplaceAll(strings.ReplaceAll(url, "https://", ""), "http://", ""), "www.", "")
+	cleanurl = strings.Split(cleanurl, "/")[0]
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:80", cleanurl), 1*time.Second)
+	if err != nil {
+		error("This URL is unreachable.")
+		return
+	}
+	defer conn.Close()
+}
+
+func message(prefix string, m string) {
+	fmt.Printf("\u001b[32;1m[%s] \u001b[0m%s\n", prefix, m)
+}
+
+func error(e string) {
+	fmt.Printf("\u001b[31;1m[!] \u001b[0m%s\n", e)
+}
+
+func prompt() {
+	fmt.Print("\u001b[36;1m\u001b[4mgid\u001b[0m > \u001b[33m")
+}
+
+func title() {
+	fmt.Println()
+	fmt.Println("\u001b[33;1m --==<[{ \u001b[37;1mGo Image Downloader \u001b[33;1m }]>==--")
+	fmt.Println()
 }
